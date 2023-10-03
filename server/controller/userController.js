9b4fb9a03cb859f656ecd269b42ea6b38705c831
Shevaitverma <shevaitverma333@gmail.com>
2023-10-03 14:43:00 +0530

    done with register user via prsima ORM

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,16 +1,30 @@
 const prisma = require("../prisma/index");
 const asyncHandler = require("express-async-handler")
 const cookieToken = require("../utils/cookieToken");
+const { json } = require("express");
 
-
+// user registeration
 const registerUser = asyncHandler(async(req, res)=>{
     const {name, email, password} = req.body;
 
+    // check if user enter data 
     if(!name, !email, !password){
         res.status(400)
         throw new Error("Please enter required fields");
     }
 
+    // check if email exists.
+    const isEmail = await prisma.user.findFirst({
+        where:{
+            email
+        }
+    })
+    if(isEmail){
+        res.status(400)
+        throw new Error("Emamil already exists")
+    }
+
+    // Creating user
     const user = await prisma.user.create({
         data:{
             name,
@@ -21,7 +35,9 @@ const registerUser = asyncHandler(async(req, res)=>{
 
     //send user data to cookie
     cookieToken(user, res);
-})
+});
+
+// user login
 
 
 
